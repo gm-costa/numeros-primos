1365855023b96e84af8e9c2b17dfe2d4f130503c
Gelson Maia <82222293+gm-costa@users.noreply.github.com>
2022-02-11 11:47:42 -0300

    Add files via upload

---
+++ primos.py
@@ -0,0 +1,24 @@
+# Crivo de Eratóstenes
+import os
+
+os.system("clear") or None  # limpa console
+
+num_maximo = int(input("Mostrar os números primos de 1 até: "))
+
+valor_limite = int(num_maximo**0.5)
+
+lista = [i for i in range(2, num_maximo + 1)]
+
+primos = []
+
+while True:
+    if lista[0] > valor_limite:
+        break
+    primos.append(lista[0])
+
+    lista = [x for x in lista if ((x % lista[0]) != 0)]
+
+primos += lista
+
+print(f"\nNúmeros primos até {num_maximo}:\n\n{primos}")
+print(f"\nQuantidade de números primos até {num_maximo}: {len(primos)}")
